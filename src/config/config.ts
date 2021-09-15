1f0e15cae65633fd3e14ad49d620d3663f9e614b
Manhcuong12345 <62935762+Manhcuong12345@users.noreply.github.com>
2021-09-15 09:33:46 +0700

    fix
    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -20,17 +20,17 @@ class ConfigSingleton {
         // firebaseToken: process.env.FIREBASE_TOKEN
     }
 
-    readonly production ={
-        jwtKey: process.env.JWT_KEY,
-        connectionString: process.env.CONNECTION_STRING,
-        connectionStringProd: process.env.CONNECTION_STRING_PROD,
-        mailAddress: process.env.MAIL_ADDRESS,
-        mailPassword: process.env.MAIL_PASSWORD,
-        thingworxHost: process.env.THINGWORX_HOST,
-        thingworxAppKey: process.env.THINGWORX_APP_KEY,
-        firebaseURL: process.env.FIREBASE_URL,
-        firebaseToken: process.env.FIREBASE_TOKEN
-    }
+    // readonly production ={
+    //     jwtKey: process.env.JWT_KEY,
+    //     connectionString: process.env.CONNECTION_STRING,
+    //     connectionStringProd: process.env.CONNECTION_STRING_PROD,
+    //     mailAddress: process.env.MAIL_ADDRESS,
+    //     mailPassword: process.env.MAIL_PASSWORD,
+    //     thingworxHost: process.env.THINGWORX_HOST,
+    //     thingworxAppKey: process.env.THINGWORX_APP_KEY,
+    //     firebaseURL: process.env.FIREBASE_URL,
+    //     firebaseToken: process.env.FIREBASE_TOKEN
+    // }
     
     //muon dung duoc thi khai bao instance de su dung va sau la doan code cuar huong dan
     public static getInstance():ConfigSingleton {
