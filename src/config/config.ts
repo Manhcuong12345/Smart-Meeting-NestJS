a5286ccbb588ebde5c9dcf291c1bd9f41a5f3066
Manhcuong12345 <62935762+Manhcuong12345@users.noreply.github.com>
2021-09-15 10:09:29 +0700

    ff

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,9 +9,9 @@ class ConfigSingleton {
     private static instance:ConfigSingleton
 
     readonly development = {
-        jwtKey: process.env.JWT_KEY,
+        // jwtKey: process.env.JWT_KEY,
         connectionString: process.env.CONNECTION_STRING,
-        connectionStringProd: process.env.CONNECTION_STRING_PROD,
+        // connectionStringProd: process.env.CONNECTION_STRING_PROD,
         // mailAddress: process.env.MAIL_ADDRESS,
         // mailPassword: process.env.MAIL_PASSWORD,
         // thingworxHost: process.env.THINGWORX_HOST,
