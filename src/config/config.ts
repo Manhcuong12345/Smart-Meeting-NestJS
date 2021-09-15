2c4c256db532f730ecabcd59d867c45a091140c9
Manhcuong12345 <62935762+Manhcuong12345@users.noreply.github.com>
2021-09-15 09:30:52 +0700

    fix
    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,12 +12,12 @@ class ConfigSingleton {
         jwtKey: process.env.JWT_KEY,
         connectionString: process.env.CONNECTION_STRING,
         connectionStringProd: process.env.CONNECTION_STRING_PROD,
-        mailAddress: process.env.MAIL_ADDRESS,
-        mailPassword: process.env.MAIL_PASSWORD,
-        thingworxHost: process.env.THINGWORX_HOST,
-        thingworxAppKey: process.env.THINGWORX_APP_KEY,
-        firebaseURL: process.env.FIREBASE_URL,
-        firebaseToken: process.env.FIREBASE_TOKEN
+        // mailAddress: process.env.MAIL_ADDRESS,
+        // mailPassword: process.env.MAIL_PASSWORD,
+        // thingworxHost: process.env.THINGWORX_HOST,
+        // thingworxAppKey: process.env.THINGWORX_APP_KEY,
+        // firebaseURL: process.env.FIREBASE_URL,
+        // firebaseToken: process.env.FIREBASE_TOKEN
     }
 
     readonly production ={
